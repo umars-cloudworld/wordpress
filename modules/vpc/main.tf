e07731125607a08afafd39322672be25c1bbbb24
umars-cloudworld <programmingknowledge03@gmail.com>
2022-01-24 12:00:00 +0500

    Added VPC Setup with SecurityGroup

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,147 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
+}
+
+#VPC Creation
+resource "aws_vpc" "web-vpc" {
+  cidr_block       = var.cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support = var.enable_dns_support
+  tags = {
+    Name = var.vpc_name
+  }
+}
+#Internet Gateway Creation and Attachment to VPC
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.web-vpc.id
+
+  tags = {
+    Name = var.igw_tag
+  }
+}
+#Public Subnet_1
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.web-vpc.id
+  cidr_block = var.public_subnet_cidr_1
+  availability_zone = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = {
+    Name = var.public_subnet_tag_1
+  }
+}
+#Public Subnet_2
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.web-vpc.id
+  cidr_block = var.public_subnet_cidr_2
+  availability_zone = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = {
+    Name = var.public_subnet_tag_2
+  }
+}
+#Database/private Subnet_1
+resource "aws_subnet" "database_subnet_1" {
+  vpc_id     = aws_vpc.web-vpc.id
+  cidr_block = var.database_subnet_cidr_1
+  availability_zone = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+
+  tags = {
+    Name = var.database_subnet_tag_1
+  }
+}
+#Database/private Subnet_2
+resource "aws_subnet" "database_subnet_2" {
+  vpc_id     = aws_vpc.web-vpc.id
+  cidr_block = var.database_subnet_cidr_2
+  availability_zone = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+
+  tags = {
+    Name = var.database_subnet_tag_2
+  }
+}
+############################### Public Route Table #######################################
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.web-vpc.id
+  tags = {
+    Name = var.public_route_table_tag
+  }
+}
+############################### Database Route Table #######################################
+
+resource "aws_route_table" "database_route_table" {
+  vpc_id = aws_vpc.web-vpc.id
+  tags = {
+    Name = var.database_route_table_tag
+  }
+}
+############################### Routes with IGW #######################################
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id            = aws_route_table.public_route_table.id
+  destination_cidr_block    = var.destination_cidr_block
+  gateway_id = aws_internet_gateway.my_igw.id
+  depends_on                = [aws_route_table.public_route_table]
+}
+
+############################### Public Subnet Association #######################################
+resource "aws_route_table_association" "public_route_table_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_route_table_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+############################### Database Subnet Association #######################################
+resource "aws_route_table_association" "database_route_table_association_1" {
+  subnet_id      = aws_subnet.database_subnet_1.id
+  route_table_id = aws_route_table.database_route_table.id
+}
+resource "aws_route_table_association" "database_route_table_association_2" {
+  subnet_id      = aws_subnet.database_subnet_2.id
+  route_table_id = aws_route_table.database_route_table.id
+}
+
+resource "aws_security_group" "web-sg" {
+  name        = "web-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.web-vpc.id
+
+  dynamic "ingress" {
+    for_each = var.rules
+    content {
+      from_port        = ingress.value["port"]
+      to_port          = ingress.value["port"]
+      protocol         = ingress.value["porto"]
+      cidr_blocks      = ingress.value["cidr_blocks"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.SG_tag
+  }
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,115 @@
 variable "region" {
   type = string
 }
+variable "cidr" {
+  description = "Enter the CIDR range required for VPC"
+  type        = string
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS Hostname"
+  type        = bool
+
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS Support"
+  type        = bool
+}
+variable "enable_ipv6" {
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block."
+  type        = bool
+
+}
+
+variable "vpc_name" {
+  description = "Tag Name to be assigned with VPC"
+  type        = string
+}
+
+
+variable "igw_tag" {
+  description = "Mention Tag needs to be associated with internet gateway"
+  type        = string
+
+}
+
+variable "public_subnet_cidr_1" {
+  description = "Cidr Blocks"
+  type        = string
+}
+
+variable "public_subnet_cidr_2" {
+  description = "Cidr Blocks"
+  type        = string
+
+}
+variable "public_subnet_tag_1" {
+  description = "Tag for public subnet"
+  type        = string
+}
+variable "public_subnet_tag_2" {
+  description = "Tag for public subnet"
+  type        = string
+}
+variable "map_public_ip_on_launch" {
+  description = "It will map the public ip while launching resources"
+  type        = bool
+
+}
+variable "database_subnet_cidr_1" {
+  description = "mention the CIDR block for database subnet"
+  type = string
+}
+variable "database_subnet_cidr_2" {
+  description = "mention the CIDR block for database subnet"
+  type = string
+}
+
+
+variable "public_route_table_tag" {
+  description = "Tag name for public route table"
+  type        = string
+
+}
+variable "destination_cidr_block" {
+  type = string
+}
+variable "database_route_table_tag" {
+  description = "Tage for database route table"
+  type        = string
+
+}
+
+variable "database_subnet_tag_1" {
+  description = "Tag for Private Subnet"
+  type        = string
+
+}
+variable "database_subnet_tag_2" {
+  description = "Tag for Private Subnet"
+  type        = string
+
+}
+variable "rules" {
+  default = [
+    {
+      port=80
+      porto="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    },
+    {
+      port=443
+      porto="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    },
+    {
+      port=22
+      porto="tcp"
+      cidr_blocks=["0.0.0.0/0"]
+    }
+  ]
+}
+variable "SG_tag" {
+  type = string
+}
